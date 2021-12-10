c413911668c948eb795838fc0b86d2f7b25f5777
Gursharn <gursharns2000@gmail.com>
2021-12-10 14:53:51 -0500

    Fixed error

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 import bodyParser from "body-parser";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import session from "express-session";
 import connectFlash from "connect-flash";
 import passport from "passport";
@@ -11,17 +11,19 @@ import path from "path";
 let app = express();
 
 //use cookie parser
-app.use(cookieParser('secret'));
+app.use(cookieParser("secret"));
 
 //config session
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: true,
     saveUninitialized: false,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 // 86400000 1 day
-    }
-}));
+      maxAge: 1000 * 60 * 60 * 24, // 86400000 1 day
+    },
+  })
+);
 
 // Enable body parser post data
 app.use(bodyParser.json());
@@ -41,3 +43,7 @@ app.use(passport.session());
 // init all web routes
 initWebRoutes(app);
 
+let port = process.env.PORT || 8080;
+app.listen(port, () =>
+  console.log(`Building a login system with NodeJS is running on port ${port}!`)
+);
